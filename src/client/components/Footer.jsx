fcd26af9dd2dfabbf382555b8c98a2b0d0a339e7
prabaprakash <prabaprakash@outlook.com>
2019-09-08 15:21:09 +0530

    #Club - Pushed Footer

--- src/client/components/Footer.jsx
+++ src/client/components/Footer.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Alert } from 'react-bootstrap'
+
 export default class Footer extends React.Component {
     constructor(props) {
         super(props);
     }
 
     render() {
-        return null;
+        return <Alert >
+            <Alert.Heading>© 2018 -2019 South Indian Trekkers </Alert.Heading>
+            <p>
+                We Connecting Souls, let's explore the western ghats and it's beauty
+       </p>
+        </Alert>
     }
 }
 
