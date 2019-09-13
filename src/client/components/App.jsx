1ae6454f8d9555ae68d8b20f94b7d4d74689580a
prabaprakash <prabaprakash@outlook.com>
2019-09-13 23:44:23 +0530

    #Club - layout and UI Changes

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash'
-import { Card, Button } from 'react-bootstrap'
+import { Card, Button, Col, Row } from 'react-bootstrap'
 import GalleryContainer from '../containers/GalleryContainer'
 export default class App extends React.Component {
   constructor(props) {
@@ -9,18 +9,20 @@ export default class App extends React.Component {
     this.renderActivitiesCards = this.renderActivitiesCards.bind(this);
   }
   renderActivitiesCards(activities) {
-    return _.map(activities, (x, i) => {
-      return <Card key={i} style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={x.image.thumbnail} />
-        <Card.Body>
-          <Card.Title>{x.title}  <small>{x.image.date}</small> </Card.Title>
-          <Card.Text className="brief">
-              {x.brief}
-          </Card.Text>
-          <Button variant="primary" onClick={() => this.props.openGallery({ id: i, openGallery: true })}>View Images</Button>
-        </Card.Body>
-      </Card>;
-    });
+    return <Row>{_.map(activities, (x, i) => {
+      return <Col key={i} sm={4}>
+          <Card>
+            <Card.Img variant="top" src={x.image.thumbnail} />
+            <Card.Body>
+              <Card.Title>{x.title}  <small>{x.image.date}</small> </Card.Title>
+              <Card.Text className="brief">
+                {x.brief}
+              </Card.Text>
+              <Button variant="primary" onClick={() => this.props.openGallery({ id: i, openGallery: true })}>View Images</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+    })}</Row>;
   }
   render() {
     return (<div className="container-fluid">
