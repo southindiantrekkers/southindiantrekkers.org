a9f7416e0038337c84585e471a8451ec0af9df4e
prabaprakash <prabaprakash@outlook.com>
2019-09-25 14:12:37 +0530

    #Club - Seo - Sitemap.xml

--- src/client/sagas/tools/storeTools.js
+++ src/client/sagas/tools/storeTools.js
@@ -7,3 +7,13 @@ _.mapValues(activities, (a, k) => {
     modifiedActivities.push(a);
 });
 fs.writeFile("modifiedActivities.json", JSON.stringify(modifiedActivities), (x, y) => console.log(x, y));
+
+
+// _.mapValues(activities, (a, k)=> {
+//     _.mapValues(a.timeline, (b, l)=> {
+//     console.log(`<url><loc>${b.photo}</loc><lastmod>2019-09-25</lastmod><changefreq>daily</changefreq><priority>0.8</priority></url>`);
+// })})
+
+// _.mapValues(activities, (a, k)=> {
+//     console.log(`<url><loc>${a.image.thumbnail}</loc><lastmod>2019-09-25</lastmod><changefreq>daily</changefreq><priority>0.8</priority></url>`);
+// });
